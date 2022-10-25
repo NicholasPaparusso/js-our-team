da2b4d05d3890b95a59179577136e37781efdbe9
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-25 19:00:16 +0200

    [end] Milestone-2-3

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -24,48 +24,74 @@ const employees =
   name: "Wayne Barnett",
   task:"Founder & CEO",
   picture:"wayne-barnett-founder-ceo.jpg",
-  email:"wayne@mail.com"
+  email:"wayne@mail.com",
+  cellNum:"2412741"
 },
 
 {
   name: "Angela Caroll",
   task:"Chief Editor",
   picture:"angela-caroll-chief-editor.jpg",
-  email:"angela@mail.com"
+  email:"angela@mail.com",
+  cellNum:"3141141"
 },
 
 {
   name: "Walter Gordon",
   task:"Office Manager",
   picture:"walter-gordon-office-manager.jpg",
-  email:"walter@mail.com"
+  email:"walter@mail.com",
+  cellNum:"8184141"
 },
 
 {
   name: "Angela Lopez",
   task:"Social Media Manager",
   picture:"angela-lopez-social-media-manager.jpg",
-  email:"angelalopez@mail.com"
+  email:"angelalopez@mail.com",
+  cellNum:"114114141"
 },
 
 {
   name: "Scott Estrada",
   task:"Developer",
   picture:"scott-estrada-developer.jpg",
-  email:"scottie@mail.com"
+  email:"scottie@mail.com",
+  cellNum:"83489141"
 },
 
 {
   name: "Barbara Ramos",
   task:"Graphic Designer",
   picture:"barbara-ramos-graphic-designer.jpg",
-  email:"brb@mail.com"
+  email:"brb@mail.com",
+  cellNum:"09230981024141"
 },
 ]
-  
+
+const carousel = document.getElementById('card-carousel');
+const btnPrev = document.getElementById('prev-btn');
+const btnNext = document.getElementById('next-btn');
+let printObject;
+let counter = 0;  
 for( let employed of employees){
   console.log(employed)
+  counter ++;
   for(let k in employed){
-    console.log( k + ": " + employed[k])
-  }
+    console.log( k + ": " + employed[k]);
+    console.log(counter)
+    printObject =
+    `
+    <div class="card position${counter}">
+      <img src="./assets/img/${employed.picture}" alt="">
+      <small class="mt-4" id="name" >${employed.name}</small>
+      <small id="task"> Founder & CEO</small>
+      <small>Contact:</small>
+      <small> ${employed.email}</small>
+      <small>${employed.cellNum}</small>
+    </div>
+    `
+  };
+  carousel.innerHTML+=printObject;
 }
+
